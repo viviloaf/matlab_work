78dcfddaa857b5a60d093a362fd5f0c784c5f735
Vinh Xuan Nguyen <vnguye34@students.kennesaw.edu>
2017-09-12 01:24:22 -0400

    create for loop that converts a string into a matrix where each letter is given a position in the matrix

---
+++ 8-31-15/forscript3.m
@@ -0,0 +1,6 @@
+b=[10 11 1 1.5 6]
+bb='holycow' %this is the name, it can be any name
+for a_string=bb
+    a_string %this is the executable part of the function
+end
+%command takes the name, ex: holycow, and gives each letter a value.

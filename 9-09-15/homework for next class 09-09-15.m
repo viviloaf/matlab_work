e8d4cefac12c9034c4162e2cbc06940f49d8ab47
Vinh Xuan Nguyen <vnguye34@students.kennesaw.edu>
2017-09-12 01:54:39 -0400

    homework

---
+++ 9-09-15/homework for next class 09-09-15.m
@@ -0,0 +1,27 @@
+homework
+
+function that makes
+
+a[i j k]
+
+b=[i j k]
+
+into
+
+c_i j = [(a_ i)*(b_ j)]
+
+
+ex: a=[1, 2, 3]
+ex: b=[3, 0.5, 0.6]
+
+takes any a and b vectors into a c vecotr
+
+example
+c= [c11=a1*b1, c12=a1*b2, c13=a1*b3]
+   [c21=a2*b1, c22=a2*b2, c23=a3*b2]
+   [c31=a3*b1, c32=a3*b2, c33=a3*b3]
+  
+  
+   use a=[1 2 3], b=[3, .5, .6]
+   
+   then create a general forumla to solve for any inputs

8c7f6951dfd05a1bcff6f84315ec35220de244d1
Vinh Xuan Nguyen <vnguye34@students.kennesaw.edu>
2017-09-12 02:22:23 -0400

    create test function to show iteration

---
+++ Final Project/itertation_test1 (1).m
@@ -0,0 +1,14 @@
+clc
+clear all
+close all
+d=50
+ee=56
+curr_itr=0
+for i=1:50
+    for j=1:56
+        total=d*ee
+        curr_itr=curr_itr+1
+        disp(['Iteration Number: ',num2str(curr_itr),' of ', num2str(total)]);
+       
+    end
+end
---
+++ Final Project/test2 (1).m
@@ -0,0 +1,5 @@
+curr_itr=1        
+while curr_itr<total
+           curr_itr=curr_itr+1
+           disp(['Iteration Number: ',num2str(curr_itr),' of ', num2str(total)]);
+        end

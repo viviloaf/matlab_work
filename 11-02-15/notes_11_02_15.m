e8d1d7d86be348b29c324974e10a7ef9b581c17f
Vinh Xuan Nguyen <vnguye34@students.kennesaw.edu>
2017-09-12 02:19:13 -0400

    create script to display homework

---
+++ 11-02-15/notes_11_02_15.m
@@ -0,0 +1,6 @@
+%   Notes 11_02_15
+%   Graphical User Interface
+disp('Subject: Engineering Vibration');disp(' ');
+disp('Vibration: Motion of mechanical objects that osciliate')
+disp('Damping of an oscillating system');
+disp('Damper: Object that impedes, reduces, or assists the movement of an object in a mechanical system');

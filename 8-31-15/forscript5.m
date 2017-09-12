cac3773fd5a648821d68efd6ac6eeec0f90c9fff
Vinh Xuan Nguyen <vnguye34@students.kennesaw.edu>
2017-09-12 01:49:37 -0400

    create for loop that add +1 to numbers in a matrix that are <5 but stops the script when the next number in the matrix does not satsify condition

---
+++ 8-31-15/forscript5.m
@@ -0,0 +1,11 @@
+b=[1 10 11 1 1.5 6]
+for a_number= b
+    if(a_number<5)
+        b_number=a_number+1
+    else
+        break %it tries only the first value, and if it is not fufiklled, 
+        %then the script stops. ex; the first value is 1, aso it can add 2,
+        %however the next value is 10, and the else command tells the
+        %script to break, and it stops the loop in the script.
+    end
+end

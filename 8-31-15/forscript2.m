b4935861a615c36c448efd17aacdda0e13f6a8aa
Vinh Xuan Nguyen <vnguye34@students.kennesaw.edu>
2017-09-12 01:23:41 -0400

    create a for loop that iterates based on amount of inputs

---
+++ 8-31-15/forscript2.m
@@ -0,0 +1,8 @@
+b=[10 11 1.5 6]
+for a_number=b
+    c=a_number+1
+end
+% for a number inputted into the above matrix, it calculates with the
+% 'c=a_number+1' as many times as there are values in the matrix. Instead
+% of specifying the number of iteriations, it takes the iterations from the
+% amount of values.

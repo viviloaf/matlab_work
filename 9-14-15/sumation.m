b5e812936d9000682521b49e757542a0c2344fb5
Vinh Xuan Nguyen <vnguye34@students.kennesaw.edu>
2017-09-12 01:55:52 -0400

    create function to find sum of a geometric series

---
+++ 9-14-15/sumation.m
@@ -0,0 +1,14 @@
+function [ s ] = sumation( N )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+%calculate the sum of the geometric series with n+1 terms
+%geometric series S= (N)(sigma)(i=0) (r^i)
+%N = amount of iterations
+%s = sum of geometric series
+
+i
+f=(r.^i)
+sum(f, i=N);
+
+end
+
